efb972da6c3ea304608fdf083fb0ec682981776c
Mirza Ahmed Hassan <138886921+Ahmeds-Library@users.noreply.github.com>
2025-04-14 11:42:37 +0500

    File Upload

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"net/http"
 	"path/filepath"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -16,8 +15,17 @@ func main() {
 			c.JSON(http.StatusBadRequest, gin.H{"message": "File not found"})
 			return
 		}
+		savePath := filepath.Join("uploads", file.Filename)
+		err = c.SaveUploadedFile(file, savePath)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Unable to save file"})
+			return
+		}
 	})
 
-	savepath := filepath.Join("upload", file.FileName)
+	r.Run(":8080")
+}
 
+func Analyze()  {
+	
 }
